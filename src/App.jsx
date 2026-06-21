a6532cf34b15398520a1812ac58a918622bee4dd
Sonu kushwaha <kushwahasonu933@gmail.com>
2026-06-21 22:53:08 +0530

    pagination and filter done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import Products from './pages/Products'
+
+const App = () => {
+  return (
+    <div>
+      <Products/>
+      
+    </div>
+  )
+}
+
+export default App
